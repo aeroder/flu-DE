bfa5a170ecf0bee9c46b558ee0a45480031b14e9
aeroder <allison.roder@nih.gov>
2021-07-14 16:44:53 -0400

    path updates for source lines and general setwd changes

--- DE_seq_start.R
+++ DE_seq_start.R
@@ -6,11 +6,11 @@
 # options(warn = 1)
 # # java heap size
 # options(java.parameters = "-Xmx8G")
-# 
+#
 # # get scripts directory (directory of this file) and load relevant functions
 # args_all = commandArgs(trailingOnly = FALSE)
 # scripts_dir = normalizePath(dirname(sub("^--file=", "", args_all[grep("^--file=", args_all)])))
-# 
+#
 # # enter source statments for functions here once ready
 # source(paste0(scripts_dir, "/load-install-packages.R"))
 # source(paste0(scripts_dir, "/deseq2-pca.R"))
@@ -18,19 +18,19 @@
 # source(paste0(scripts_dir, "/plot-volcano.R"))
 # source(paste0(scripts_dir, "/plot-heatmap.R"))
 
-source('~/Dropbox/Ghedin_Lab/DARPA_Seq/R_files/functions/heatmap-AR.R')
-source('~/Dropbox/Ghedin_Lab/DARPA_Seq/R_files/functions/deseq2_analysis.R')
+source('~/National Institutes of Health/Ghedin, Elodie (NIH NIAID) [E] - LAB_STUFF/allison/projects/DARPA_seq/R_files/flu-DE/heatmap-AR.R')
+source('~/National Institutes of Health/Ghedin, Elodie (NIH NIAID) [E] - LAB_STUFF/allison/projects/DARPA_seq/R_files/flu-DE/deseq2_analysis.R')
 
 # # relevent arguments, only take arguments after the --args flag
 # args = commandArgs(trailingOnly = TRUE)
 # counts_table_file = args[1]
 # col_data_file = args[2]
-# 
+#
 # # check to make sure there are 2 arguments provided
 # if (length(args) < 2) {
 #   stop("Must provide a count table and a metadata file")
 # }
-# 
+#
 # # check that input files exist
 # if (!file.exists(counts_table_file)) stop("file does not exist: ", counts_table_file)
 # if (!file.exists(col_data_file)) stop("file does not exist: ", col_data_file)
@@ -63,8 +63,7 @@ deseq_start <- function(count_table, column_data, r_dir, group_1 = 1, group_2 = 
     # ----------------- Input importing --------------------
 
     # import counts table
-    setwd("~/Dropbox/Ghedin_Lab/DARPA_Seq")
-    counts_table = read.csv(file = glue("./count_Tables/processed_countdata/{count_table}"), row.names = 1, 
+    counts_table = read.csv(file = glue("./{count_table}"), row.names = 1,
                             stringsAsFactors = FALSE, check.names = FALSE)
     message("input counts table gene num:      ", nrow(counts_table))
     message("input counts table sample num:    ", ncol(counts_table))
@@ -72,7 +71,7 @@ deseq_start <- function(count_table, column_data, r_dir, group_1 = 1, group_2 = 
     message("")
 
     # import groups table
-    col_data = read.csv(file = glue("./count_Tables/processed_coldata/{column_data}"), 
+    col_data = read.csv(file = glue("./{column_data}"),
                         header = TRUE, row.names = 1, colClasses = "factor")
     message("metadata row num:   ", nrow(col_data))
     message("metadata condition names: ", toString(rownames(col_data)))
@@ -80,10 +79,10 @@ deseq_start <- function(count_table, column_data, r_dir, group_1 = 1, group_2 = 
     message("")
 
     #readline("Enter file prefix for directory name and saving (note: no spaces): ")
-    if (!dir.exists(glue("~/Dropbox/Ghedin_Lab/DARPA_Seq/DESeq_runs/{r_dir}"))) {
-      dir.create(glue("~/Dropbox/Ghedin_Lab/DARPA_Seq/DESeq_runs/{r_dir}"))
+    if (!dir.exists(glue("./{r_dir}"))) {
+      dir.create(glue("./{r_dir}"))
     }
-    setwd(glue("~/Dropbox/Ghedin_Lab/DARPA_Seq/DESeq_runs/{r_dir}"))
+    setwd(glue("./{r_dir}"))
 
     # check that all samples from the column data are found in the counts table
     diff_samples = setdiff(rownames(col_data), colnames(counts_table))
@@ -111,7 +110,7 @@ deseq_start <- function(count_table, column_data, r_dir, group_1 = 1, group_2 = 
     message("group levels: ", toString(group_levels))
     message("secondary group levels: ", toString(gb_levels))
     message("")
-  
+
     contrast <- c(group_a, toString(group_levels[2]), toString(group_levels[1]))
 
     # design formula - what should DESeq ultimately compare between?
@@ -133,24 +132,24 @@ deseq_start <- function(count_table, column_data, r_dir, group_1 = 1, group_2 = 
     vsd = varianceStabilizingTransformation(dds, blind = TRUE)
 
     # export normalized counts
-    norm_counts = counts(dds, normalized = TRUE) %>% 
-      round(2) %>% 
-      as.data.frame() %>% 
+    norm_counts = counts(dds, normalized = TRUE) %>%
+      round(2) %>%
+      as.data.frame() %>%
       rownames_to_column("gene")
     write_excel_csv(norm_counts, glue("{r_dir}_normcounts.csv"))
     write_xlsx(list(normalized_counts = norm_counts), path = glue("{r_dir}_normcounts.xlsx"))
 
     # export variance stabilized counts
-    vsd_table = assay(vsd) %>% 
-      round(2) %>% 
-      as.data.frame() %>% 
+    vsd_table = assay(vsd) %>%
+      round(2) %>%
+      as.data.frame() %>%
       rownames_to_column("gene")
     write_excel_csv(vsd_table, path = glue("{r_dir}_vstcounts.csv"))
 
     # ----------------------- PCA ------------------------
 
     # PCA plot
-    source('~/Dropbox/Ghedin_Lab/DARPA_Seq/R_files/functions/deseq2-pca.R')
+    source('~/National Institutes of Health/Ghedin, Elodie (NIH NIAID) [E] - LAB_STUFF/allison/projects/DARPA_seq/R_files/flu-DE/deseq2-pca.R')
     pca_plot = deseq2_pca(vsd, intgroup = c(group_a, group_b), ntop = 1000)
     save_plot(glue("{r_dir}_pca.pdf"), pca_plot, base_width = 8, base_height = 5, units = "in", dpi = 300)
     save_plot(glue("{r_dir}_pca.png"), pca_plot, base_width = 8, base_height = 5, units = "in", dpi = 300)
@@ -159,7 +158,7 @@ deseq_start <- function(count_table, column_data, r_dir, group_1 = 1, group_2 = 
     # ---------------differential expression------------------
 
     # perform comparisons for all results in the results table
-    source('~/Dropbox/Ghedin_Lab/DARPA_Seq/R_files/functions/deseq2_analysis.R')
+    source('~/National Institutes of Health/Ghedin, Elodie (NIH NIAID) [E] - LAB_STUFF/allison/projects/DARPA_seq/R_files/flu-DE/deseq2_analysis.R')
     res_names <- as.list(resultsNames(dds))
     res_names <- res_names[-c(1)]
     for (result in seq_along(res_names)) {
@@ -167,7 +166,7 @@ deseq_start <- function(count_table, column_data, r_dir, group_1 = 1, group_2 = 
       message(glue("Comparison: ", res))
       deseq2_analysis(deseq_data = dds, name = res_names[[result]], contrast = contrast, save_plot = TRUE)
     }
-    
+
     if (returnDDS == TRUE) {
         return(dds)
     }
--- deseq2_analysis.R
+++ deseq2_analysis.R
@@ -1,9 +1,9 @@
 
 deseq2_analysis <- function(deseq_data, contrast = NULL, name = NULL, r_dir = NULL, ...) {
-  
+
   # For DESeq2 analysis
   library("DESeq2")
-  
+
   # For data maniputation and saving
   library("RColorBrewer")
   library("glue")
@@ -12,14 +12,14 @@ deseq2_analysis <- function(deseq_data, contrast = NULL, name = NULL, r_dir = NU
   library("tidyr")
   library("dplyr")
   library("tibble")
-  
+
   # For graphing and dataviz
   library("VennDiagram")
   library("biobroom")
   library("ggplot2")
   library("ggfortify")
   library("pheatmap")
-  
+
   # create a directory to store the output files
   r_dir <- getwd()
   if(!is.null(r_dir)) {
@@ -27,26 +27,26 @@ deseq2_analysis <- function(deseq_data, contrast = NULL, name = NULL, r_dir = NU
   } else {
     r_dir <- getwd()
   }
-  
+
   # Make a directory to store heatmaps in
   heatmaps_dir = "heatmaps"
   if (!dir.exists(glue("{r_dir}/{heatmaps_dir}"))) {
     dir.create(glue("{r_dir}/{heatmaps_dir}"))
   }
-  
+
   # Make a directory to store volcano plots in
   volcano_dir = "volcano-plots"
   if (!dir.exists(glue("{r_dir}/{volcano_dir}"))) {
     dir.create(glue("{r_dir}/{volcano_dir}"))
   }
-  
-  # Make a directory to store venns diagrams in 
+
+  # Make a directory to store venns diagrams in
   vennD_dir = "venndiagrams"
   if (!dir.exists(glue("{r_dir}/{vennD_dir}"))) {
     dir.create(glue("{r_dir}/{vennD_dir}"))
   }
-  
-  # generate a results table based on the supplied name. 
+
+  # generate a results table based on the supplied name.
   # If no name is given, stop the analysis and return an error
   if(!is.null(name)) {
     res <- results(deseq_data, name = name, cooksCutoff = FALSE)
@@ -54,23 +54,23 @@ deseq2_analysis <- function(deseq_data, contrast = NULL, name = NULL, r_dir = NU
   } else {
     stop("Please supply a name for the results table to be generated.")
   }
-  
+
   # DESeq results name format : "Virus_FluB_vs_Mock
   # get the file suffix based on comparison name
   file_suffix = gsub(pattern = "Virus_", replacement = "", x = res_name)
   file_suffix = gsub(pattern = "_vs_Mock", replacement = "", x = file_suffix)
-  
+
   # save unmodified results object
   res_rds = glue("{r_dir}/deseq2_res_{file_suffix}.rds")
   saveRDS(res, file = res_rds)
   message("saving results object: ", res_rds)
-  
+
   # save unmodified results as csv
   res_df <- as.data.frame(res) %>% rownames_to_column("gene")
   res_csv <- glue("res_{file_suffix}.csv")
   write_excel_csv(res_df, path = res_csv)
   message("saving results csv: ", res_csv)
-  
+
   # format results and save as excel spreadsheet
   res_df <- res_df %>%
     dplyr::mutate(
@@ -82,44 +82,45 @@ deseq2_analysis <- function(deseq_data, contrast = NULL, name = NULL, r_dir = NU
     dplyr::rename(
       log2FC = log2FoldChange
     )
-  
-    res_df <- res_df %>% 
+
+    res_df <- res_df %>%
       dplyr::select(gene, baseMean, log2FC, pvalue, padj)
 
     res_xlsx <- glue("{file_suffix}_res.xlsx")
     write_xlsx(setNames(list(res_df), strtrim(res_name, 31)), path = res_xlsx)
     message("saving results as excel file: ", res_xlsx)
-  
+
     # generate volcano plot
-    plot_volcano(stats_df = res_df,  
+    source('~/National Institutes of Health/Ghedin, Elodie (NIH NIAID) [E] - LAB_STUFF/allison/projects/DARPA_seq/R_files/flu-DE/plot-volcano_YH.R')
+    plot_volcano(stats_df = res_df,
                  positive_group=contrast[2],
-                 negative_group=contrast[3], 
-                 gene_col = "gene", 
-                 fc_col = "log2FC", 
-                 p_col = "padj", 
+                 negative_group=contrast[3],
+                 gene_col = "gene",
+                 fc_col = "log2FC",
+                 p_col = "padj",
                  p_cutoff = 0.05,
-               fc_label = "Fold Change (log2)", p_label = "Adjusted P Value (-log10)",
-               title = res_name,
-               file_prefix = glue("{volcano_dir}/plot.volcano2.{file_suffix}"))
+                 fc_label = "Fold Change (log2)", p_label = "Adjusted P Value (-log10)",
+                 title = res_name,
+                 file_prefix = glue("{volcano_dir}/plot.volcano2.{file_suffix}"))
     
     message("# genes padj < 0.1: ", nrow(subset(res_df, padj < 0.1)))
     message("# genes padj < 0.05: ", nrow(subset(res_df, padj < 0.05)))
     message("# genes padj < 0.01: ", nrow(subset(res_df, padj < 0.01)))
     message("# genes padj < 0.001: ", nrow(subset(res_df, padj < 0.001)))
-    
+
     # save differential expression results in Excel format
     res_q005_xlsx <- gsub(pattern = ".xlsx", replacement = "_q005.xlsx", x = res_xlsx)
     res_q005_df <- subset(res_df, padj < 0.05)
     write_xlsx(setNames(list(res_q005_df), strtrim(res_name, 31)), path = res_q005_xlsx)
     message("saving DE genes results as excel file: ", res_q005_xlsx)
-    
-    # perform vst on data for use in heatmap 
+
+    # perform vst on data for use in heatmap
     vst <- assay(varianceStabilizingTransformation(deseq_data, blind = TRUE))
     samples_all = colnames(deseq_data)
     betas <- coef(deseq_data)
-    
+
     # heatmap gene subsets (list with genes, plot title, and file suffix)
-    # Heatmap code written by Yuhan! 
+    # Heatmap code written by Yuhan!
     hmg = list()
     hmg[[length(hmg) + 1]] = list(genes = res_df %>% dplyr::filter(padj < 0.05) %>% .$gene,
                                   title = "q < 0.05",
@@ -136,28 +137,28 @@ deseq2_analysis <- function(deseq_data, contrast = NULL, name = NULL, r_dir = NU
     hmg[[length(hmg) + 1]] = list(genes = res_df %>% dplyr::filter(pvalue < 0.01) %>% .$gene,
                                   title = "p < 0.01",
                                   file_suffix = "p001")
-    
+
     # process every gene subset
     for (i in 1:length(hmg)) {
-      
+
       # generate title and file suffix
       hm_title = glue("{res_name}_{hmg[[i]]$title}")
       hm_file_prefix = glue("{r_dir}/{heatmaps_dir}/hm_{file_suffix}_{hmg[[i]]$file_suffix}")
-      
+
       # generate heatmaps if gene list is not too small or big
       if (length(hmg[[i]]$genes) > 10 && length(hmg[[i]]$genes) < 10000) {
-        
+
         mat <- betas[hmg[[i]]$genes,-c(1)]
         # generate heatmap using all samples
-        
-        heatmapAR(mat = mat, 
+
+        heatmapAR(mat = mat,
                   row_subset = hmg[[i]]$genes,
-                  title = hm_title, 
+                  title = hm_title,
                   file_prefix = hm_file_prefix,
                   save_plot = TRUE)
-        
+
       }
     }
-    
+
 
 }
